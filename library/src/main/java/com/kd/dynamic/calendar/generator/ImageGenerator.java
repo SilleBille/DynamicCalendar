c61a7c3f1bbbbc6ec685e57e5282a0d43576a286
Dinesh Prasanth <mkdineshprasanth@gmail.com>
2015-01-06 19:47:33 +0530

    Changed the name of the module to Library

--- library/src/main/java/com/kd/dynamic/calendar/generator/ImageGenerator.java
+++ library/src/main/java/com/kd/dynamic/calendar/generator/ImageGenerator.java
@@ -1,9 +1,42 @@
+/*
+ * Copyright (c) 2015. DineshPrasanth
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.kd.dynamic.calendar.generator;
 
-/**
- * Created by DineshPrasanth on 06-01-2015.
- */
+
+import android.content.Context;
+import android.graphics.Color;
+
 public class ImageGenerator {
+
+    private Context mContext;
+    private float mSize;
+    private Color mColor;
+
+    public ImageGenerator(Context context) {
+        mContext = context;
+    }
+
+    public void setFont(float size) {
+        mSize = size;
+    }
+
+    public void setmColor(Color color) {
+        mColor = color;
+    }
 
    /* public Bitmap generateImage(Context context, String month, String date) {
         Bitmap src = BitmapFactory.decodeResource(context.getResources(), R.drawable.calendar_empty); // the original file yourimage.jpg i added in resources
