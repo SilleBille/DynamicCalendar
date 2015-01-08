2beae302690fa522abaf9fe24ddcd987bbe6d97f
Dinesh Prasanth <mkdineshprasanth@gmail.com>
2015-01-08 22:04:30 +0530

    Random fix

--- sample/src/main/java/com/kd/dynamic/calendar/MainActivity.java
+++ sample/src/main/java/com/kd/dynamic/calendar/MainActivity.java
@@ -21,7 +21,6 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -69,8 +68,6 @@ public class MainActivity extends ActionBarActivity {
                         mDisplayGeneratedImage.setImageBitmap(mGeneratedDateIcon);
 
 
-                        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams();
-                        mTopLine.setLayoutParams();
                     }
                 }, mYear, mMonth, mDay);
                 mDatePicker.show();
